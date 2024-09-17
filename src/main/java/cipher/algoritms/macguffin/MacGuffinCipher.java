3e33c4713d802455a14510633bddec5ebed4c28e
Nikita Smirnov <smirnov.nikita64@gmail.com>
2024-09-18 00:50:21 +0300

    MacGuffin works perfect

--- src/main/java/cipher/algoritms/macguffin/MacGuffinCipher.java
+++ src/main/java/cipher/algoritms/macguffin/MacGuffinCipher.java
@@ -8,129 +8,173 @@ public class MacGuffinCipher implements ISymmCipher, IKeyExpansion {
 
     private final int rounds = 32;
 
-    private final int keySize = rounds * 3;
+    private char[][] keysRounds = new char[32][3];
 
-    private final char[] keys = new char[keySize];
-
-    private final int tSize = 1 << 16;
-
-    private final int[] sTable = new int[tSize];
-
-    private int getIndexSBox(int i, int j){
-        int result = 0;
-        for (int k = 0; k < 6; k++) {
-            result |= ((i >>> MagicConstants.sBits[j][k]) & 1) << k;
-        }
-        return result;
-    }
-
-    private void initTable(){
-        for(int i = 0; i < tSize; i++){
-            for (int j = 0; j < 8; j++) {
-                sTable[i] |= MagicConstants.sBoxes[j][getIndexSBox(i, j)];
-            }
-        }
-    }
-
-    public MacGuffinCipher(byte[] key){
+    public MacGuffinCipher(byte[] key) {
         setKey(key);
-    }
-
-    private char getRforRound(char a, char b, char c){
-        return (char) (
-                (MagicConstants.OUT0 & sTable[(a & MagicConstants.IN00) | (b & MagicConstants.IN00) | (c & MagicConstants.IN00)]) |
-                (MagicConstants.OUT1 & sTable[(a & MagicConstants.IN10) | (b & MagicConstants.IN11) | (c & MagicConstants.IN12)]) |
-                (MagicConstants.OUT2 & sTable[(a & MagicConstants.IN20) | (b & MagicConstants.IN21) | (c & MagicConstants.IN22)]) |
-                (MagicConstants.OUT3 & sTable[(a & MagicConstants.IN30) | (b & MagicConstants.IN31) | (c & MagicConstants.IN32)])
-        );
-    }
-
-    private byte[] getBytes(byte[] initial, char[] r) {
-        for (int j = 0; j < 8; j++) {
-            initial[j] = (j & 1) == 0 ? (byte) (r[j / 2]) : (byte) (r[(j - 1) / 2]);
-
-        }
-        return initial;
     }
 
     @Override
     public void setKey(byte[] key) {
-        int i, j;
-        byte[][] k = new byte[2][8];
-        initTable();
-
-        System.arraycopy(key, 0, k[0], 0, 8);
-        System.arraycopy(key, 8, k[1], 0, 8);
-
-        for (i = 0; i < keySize; i++) {
-            keys[i] = 0;
-        }
-
-        for (i = 0; i < 2; i++) {
-            for (j = 0; j < 32; j++) {
-                encryptBlock(k[i]);
-                keys[j * 3] ^= (char) ((k[i][0] & 0xFF) | ((k[i][1] & 0xFF) << 8));
-                keys[j * 3 + 1] ^= (char) ((k[i][2] & 0xFF) | ((k[i][3] & 0xFF) << 8));
-                keys[j * 3 + 2] ^= (char) ((k[i][4] & 0xFF) | ((k[i][5] & 0xFF) << 8));
-            }
-        }
+        setKeyKey(key);
     }
 
     @Override
     public byte[] encryptBlock(byte[] initial) {
-        char[] r = new char[4];
-
-        for (int i = 0; i < 4; i++) {
-            r[i] = (char) (initial[i * 2] & 0xFF | (initial[i * 2 + 1] << 8));
-        }
-//        char r0 = (char) ((initial[0] | (initial[1] << 8)));
-//        char r1= (char) ((initial[2] | (initial[3] << 8)));
-//        char r2 = (char) (initial[4] | (initial[5] << 8));
-//        char r3 = (char) (initial[6] | (initial[7] << 8))
-        char a, b, c, i = 0, counter = 0;
-        for (; i < (rounds / 4);i++) {
-            for (int j = 0; j < 4; j++){
-                a = (char) (r[(j + 1) % 4] & keys[counter++]);
-                b = (char) (r[(j + 2) % 4] & keys[counter++]);
-                c = (char) (r[(j + 3) % 4] & keys[counter++]);
-                r[j] ^= getRforRound(a, b, c);
-            }
-//            a = (char) (r1 & keys[counter++]);
-//            b = (char) (r2 & keys[counter++]);
-//            c = (char) (r3 & keys[counter++]);
-//            r0 ^= getRforRound(a, b, c);
-//            a = (char) (r2 & keys[counter++]);
-//            b = (char) (r3 & keys[counter++]);
-//            c = (char) (r0 & keys[counter++]);
-//            r1 ^= getRforRound(a, b, c);
-//            a = (char) (r3 & keys[counter++]);
-//            b = (char) (r0 & keys[counter++]);
-//            c = (char) (r1 & keys[counter++]);
-//            r2 ^= getRforRound(a, b, c);
-//            a = (char) (r0 & keys[counter++]);
-//            b = (char) (r1 & keys[counter++]);
-//            c = (char) (r2 & keys[counter++]);
-//            r3 ^= getRforRound(a, b, c);
-        }
-        return getBytes(initial, r);
+        return encryptBlock(initial, keysRounds);
     }
 
     @Override
     public byte[] decryptBlock(byte[] initial) {
-        char[] r = new char[4];
-        for (int i = 0; i < 4; i++) {
-            r[i] = (char) (initial[i * 2] | (initial[i * 2 + 1] << 8));
-        }
-        char a, b, c, i = 0, counter = keySize;
-        for (; i < (rounds / 4);i++) {
-            for (int j = 3; j > -1; j--){
-                a = (char) (r[(j + 1) % 4] & keys[--counter]);
-                b = (char) (r[(j + 2) % 4] & keys[--counter]);
-                c = (char) (r[(j + 3) % 4] & keys[--counter]);
-                r[j] ^= getRforRound(a, b, c);
-            }
+        return decryptBlock(initial, keysRounds);
+    }
+
+    private byte getBitForPosition(char word, int position) {
+        return (byte) ((word >> position) & 1);
+    }
+
+    public byte getOuterBits(byte word, int size) {
+        int left = word >> (size - 1);
+        int right = word & 1;
+        return (byte) ((left << 1) | right);
+    }
+
+    public byte getInnerBits(byte word, int size) {
+        return (byte) ((word >> 1) & 0b1111);
+    }
+
+    public byte getSixBitsFromSBox(char a, char b, char c, int sBoxNum) {
+        byte result = 0;
+        int count = 2;
+        char[] collected = {a, b, c};
+        int[] places = MagicConstants.sBits[sBoxNum];
+        for (int i = 5; i >= 0; i -= 2) {
+            char currentChar = collected[count--];
+            result <<= 1;
+            result |= getBitForPosition(currentChar, places[i]);
+            result <<= 1;
+            result |= getBitForPosition(currentChar, places[i - 1]);
 
         }
-        return getBytes(initial, r);
+        return result;
     }
+
+    // works consistent
+    public char[] bytesToChars(byte[] byteArray) {
+        if (byteArray.length != 8) {
+            throw new IllegalArgumentException("Array must be exactly 8 bytes long.");
+        }
+
+        char[] chars = new char[4];
+
+        for (int i = 0; i < 4; i++) {
+            // Каждое слово char состоит из двух байт
+            int index = i * 2;
+            // Объединяем два байта в один char
+            chars[i] = (char) (((byteArray[index] & 0xFF) << 8) | (byteArray[index + 1] & 0xFF));
+        }
+
+        return chars;
+    }
+
+    // works consistant
+    public byte[] charsToBytes(char[] charArray) {
+        // Массив байтов должен быть в два раза длиннее массива символов
+        byte[] byteArray = new byte[charArray.length * 2];
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            byteArray[i * 2] = (byte) ((charArray[i] >> 8) & 0xFF);
+
+            byteArray[i * 2 + 1] = (byte) (charArray[i] & 0xFF);
+        }
+
+        return byteArray;
+    }
+
+    public long byteArrayToLong(byte[] byteArray) {
+        if (byteArray.length != 8) {
+            throw new IllegalArgumentException("Массив должен содержать 8 байт");
+        }
+
+        long result = 0;
+        for (int i = 0; i < 8; i++) {
+            result = (result << 8) | (byteArray[i] & 0xFF);
+        }
+
+        return result;
+    }
+
+    public byte[] encryptBlock(byte[] block, char[][] key) {
+        char[] words = bytesToChars(block);
+        char left = words[0], a = words[1], b = words[2], c = words[3];
+        for (int i = 0; i < rounds; i++) {
+            left = getLeftForRound(a, b, c, key, left, i);
+            char tmp = left;
+            left = a;
+            a = b;
+            b = c;
+            c = tmp;
+        }
+        return charsToBytes(new char[]{left, a, b, c});
+    }
+
+    private byte changeBytes(byte word){
+        return (byte) ((word >> 1) | ((word & 1) << 1));
+    }
+
+    public byte[] decryptBlock(byte[] block, char[][] key) {
+        char[] words = bytesToChars(block);
+        char left = words[3], a = words[0], b = words[1], c = words[2];
+//        char left = words[1], a = words[2], b = words[3], c = words[0];
+        for (int i = 31; i >= 0; i--) {
+            left = getLeftForRound(a, b, c, key, left, i);
+            char tmp = left;
+            char tmpa = a;
+            char tmpb = b;
+            left = c;
+            a = tmp;
+            b = tmpa;
+            c = tmpb;
+        }
+        return charsToBytes(new char[]{a, b, c, left});
+    }
+
+    // works consistant
+    private char getLeftForRound(char a, char b, char c, char[][] key, char left, int index) {
+        char t = 0;
+        for (int j = 0; j < 8; j++) {
+            byte positions = getSixBitsFromSBox((char) (a ^ key[index][0]), (char) (b ^ key[index][1]), (char) (c ^ key[index][2]), j);
+            byte bitsToHav = (byte) MagicConstants.gufnSBox[j][positions];
+            bitsToHav = changeBytes(bitsToHav);
+
+            t |= (char) (bitsToHav << (j * 2));
+        }
+        left ^= t;
+        return left;
+    }
+
+    // seem right
+    public void setKeyKey(byte[] key){
+        byte[] leftKey = new byte[8];
+        byte[] rightKey = new byte[8];
+        System.arraycopy(key, 0, leftKey, 0, 8);
+        System.arraycopy(key, 8, rightKey, 0, 8);
+        for (int i = 0; i < 32; i++) {
+            leftKey = encryptBlock(leftKey, keysRounds);
+            char[] keysChars = bytesToChars(leftKey);
+            keysRounds[i][0] = keysChars[0];
+            keysRounds[i][1] = keysChars[1];
+            keysRounds[i][2] = keysChars[2];
+        }
+        for (int i = 0; i < 32; i++) {
+            rightKey = encryptBlock(rightKey, keysRounds);
+            char[] keysChars = bytesToChars(rightKey);
+            keysRounds[i][0] = (char) (keysRounds[i][0] ^ keysChars[0]);
+            keysRounds[i][1] = (char) (keysRounds[i][1] ^ keysChars[1]);
+            keysRounds[i][2] = (char) (keysRounds[i][2] ^ keysChars[2]);
+        }
+
+    }
+
 }
